ebdcfe6a784a9ed3dfc803e2a8dd4c46790a57f3
Matthieu ROUSSEAU <rousseaumatth@orange.fr>
2020-06-22 15:47:23 +0200

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/matthieu/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/matthieu/catkin_ws/devel;/home/matthieu/ws_moveit/devel;/opt/ros/kinetic".split(';') if "/home/matthieu/catkin_ws/devel;/home/matthieu/ws_moveit/devel;/opt/ros/kinetic" != "" else []
---
+++ build/kevin_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/matthieu/catkin_ws/src/kevin_gazebo
+# Build directory: /home/matthieu/catkin_ws/build/kevin_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
